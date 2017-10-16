c5aba7ec183c6f628c5bf74643bb40855c9da282
zimod <andy199354@gmail.com>
2017-10-16 14:20:49 -0700

    added the array of quote objects

---
+++ js/quotes.js
@@ -0,0 +1,29 @@
+// create an array of quote objects called quotes with multiple properties
+
+var quotes = [
+ {  quote:"I love you the more in that I believe you had liked me for my own sake and for nothing else.",
+   source:"John Keats",
+   tags:["Love","Yourself"]
+ },
+ { quote:"Life isn't about finding yourself. Life is about creating yourself.",
+   source:"George Bernard Shaw",
+   tags:["Love","Yourself","Finding"]
+ },
+ { quote:"Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.",
+   source:"Buddha",
+   tags:["Life","Future","Past"]
+ },
+ { quote:"Education is the most powerful weapon which you can use to change the world.",
+   source:"Nelson Mandela",
+   year:"2003",
+   tags:["Life","Future","Past"]
+ },
+ { quote:"We must let go of the life we have planned, so as to accept the one that is waiting for us.",
+   source:"Joseph Campbell",
+   tags:["Life","Inspirational","Waiting"]
+ },
+ { quote:"The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
+   source:"Helen Keller",
+   tags:["Inspirational","Beautiful"]
+ },
+];
