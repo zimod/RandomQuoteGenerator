eae74a671edc0a26fea0aca3830af06a22a62618
zimod <andy199354@gmail.com>
2017-10-16 18:01:01 -0700

    minior comment updates

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote , false);
+
 var clone = quotes.slice();/*this clones the quotes array so that we can later on remove objects
 from the clone to aviod duplicates*/
 
